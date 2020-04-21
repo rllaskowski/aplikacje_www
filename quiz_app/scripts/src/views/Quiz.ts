60c48b791a73f2494e397aa2fd0122a7f96ef651
rllaskowski <robert.laskowski0@gmail.com>
2020-04-21 21:29:01 +0200

    update

--- quiz_app/scripts/src/views/Quiz.ts
+++ quiz_app/scripts/src/views/Quiz.ts
@@ -34,7 +34,7 @@ let answeredCount: number;
 
 // View renderers
 const renderTimer = () => {
-    timer.innerText = `${timeElapsed}s`;
+    timer.innerText = `${timeElapsed} sek`;
 }
     
 const renderQuestion = () => {
