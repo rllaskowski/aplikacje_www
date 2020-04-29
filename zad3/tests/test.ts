356d13ccb3fa9af40892638764ec1833aa3fa300
rllaskowski <robert.laskowski0@gmail.com>
2020-04-29 14:08:46 +0200

    tests

--- zad3/tests/test.ts
+++ zad3/tests/test.ts
@@ -1,7 +1,6 @@
-import { fib } from "../scripts/script"
+import { fib } from "../scripts/script";
 import { expect } from "chai";
 import "mocha";
-
 
 describe("Fibonacci", () => {
     it("should equal 0 for call with 0", () => {
